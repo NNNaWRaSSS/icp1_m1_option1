9088e8a47726144686b66c73e69fc1cd632dab91
Ricardo Lozano <ricardo.lozano.quesada@gmail.com>
2024-10-01 16:15:50 +0200

    Added comments

--- src/myprogram.c
+++ src/myprogram.c
@@ -8,27 +8,38 @@ double circle_area(double radius);
 double triangle_perimeter(double side1, double side2, double side3);
 double triangle_area(double base, double height);
 
-// Function definitions. Return -1 if the input is invalid
+//Fill in with the correct formulas to calculate the perimeter of a rectangle
+//Return -1 if the input is invalid
 double rectangle_perimeter(double length, double width) {
     return -1;
 }
 
+//Fill in with the correct formulas to calculate the area of a rectangle
+//Return -1 if the input is invalid
 double rectangle_area(double length, double width) {
     return -1;
 }
 
+//Fill in with the correct formulas to calculate the perimeter of a circle
+//Return -1 if the input is invalid
 double circle_perimeter(double radius) {
     return -1;
 }
 
+//Fill in with the correct formulas to calculate the area of a circle
+//Return -1 if the input is invalid
 double circle_area(double radius) {
     return -1;
 }
 
+//Fill in with the correct formulas to calculate the perimeter of a triangle
+//Return -1 if the input is invalid
 double triangle_perimeter(double side1, double side2, double side3) {
     return -1;
 }
 
+//Fill in with the correct formulas to calculate the area of a triangle
+//Return -1 if the input is invalid
 double triangle_area(double base, double height) {
     return -1;
 }
